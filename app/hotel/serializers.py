c5d692a4e460f775e8bf05b4d0eebdcfb8c79513
Pilkevych Oleksii <oleksii.pilkevych@nokia.com>
2018-12-15 11:01:14 +0100

    Added endpoints

--- app/hotel/serializers.py
+++ app/hotel/serializers.py
@@ -6,3 +6,13 @@ class RodzajeSprzetuSerializer(serializers.ModelSerializer):
     class Meta:
         model = RodzajSprzetu
         fields = '__all__'
+
+
+def create_serializer(base_model):
+    class HotelSerializer(serializers.ModelSerializer):
+        class Meta:
+            model = base_model
+            fields = '__all__'
+    return HotelSerializer
+
+
--- app/hotel/urls.py
+++ app/hotel/urls.py
@@ -2,10 +2,13 @@ from django.conf.urls import url
 from django.urls import include
 from rest_framework import routers
 
-from .views import RodzajeSprzetuView
+from .views import views
 
 router = routers.DefaultRouter()
-router.register(r'rodzaje_sprzetu', RodzajeSprzetuView)
+# router.register(r'rodzaje_sprzetu', RodzajeSprzetuView)
+for view in views:
+    router.register(view.serializer_class.Meta.model.__name__, view)
+
 
 urlpatterns = [
     url(r'^', include(router.urls))
